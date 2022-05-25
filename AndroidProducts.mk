5711dbc3d379c8fc7eaa29b4b4cc1dbda9f72e64
vytautas <vytautas.duchas@gmail.com>
2022-05-25 19:08:04 +0300

    velcome dotOS (1/2)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
-#
+    #
 # Copyright (C) 2018-2021 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cipher_vayu.mk
+    $(LOCAL_DIR)/dot_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    cipher_vayu-eng \
-    cipher_vayu-userdebug \
-    cipher_vayu-user
+    dot_vayu-eng \
+    dot_vayu-userdebug \
+    dot_vayu-user
