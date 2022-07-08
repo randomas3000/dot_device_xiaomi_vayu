8079570f482a2f4e4f626964d8f278ae317d74cb
vytautas <vytautas.duchas@gmail.com>
2022-07-09 02:00:18 +0300

    initial bringup for yalp [2/2]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_vayu-eng \
-    cherish_vayu-userdebug \
-    cherish_vayu-user
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
